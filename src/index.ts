2fb199065b9d6d38a0b190e6055787a27c63775e
Guilherme Araújo <arauujogui@gmail.com>
2024-12-12 22:00:26 -0300

    Create initial transpiler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log("Hello, world!");
+import { PureAbility } from "@casl/ability";
+import { rulesToAST } from "@casl/ability/extra";
+import { FieldCondition } from "@ucast/core";
+import { eq, gt, SQL } from "drizzle-orm";
+import { type PgTableWithColumns, type TableConfig } from "drizzle-orm/pg-core";
+
+type Operator = (
+  condition: FieldCondition,
+  table: PgTableWithColumns<TableConfig>,
+) => SQL | undefined;
+
+const operators: Record<string, Operator> = {
+  eq: (condition, table) => {
+    const column = table[condition.field];
+
+    return eq(column, condition.value);
+  },
+  gt: (condition, table) => {
+    const column = table[condition.field];
+
+    return gt(column, condition.value);
+  },
+};
+
+export function rulesToDrizzle<T extends TableConfig>(
+  ability: PureAbility,
+  table: PgTableWithColumns<T>,
+): SQL | undefined {
+  const condition = rulesToAST(ability, "read", "User") as FieldCondition;
+
+  if (!condition) {
+    return undefined;
+  }
+
+  const { operator } = condition;
+
+  const op = operators[operator];
+
+  if (!op) {
+    throw new Error(`Unsupported operator: ${operator}`);
+  }
+
+  return op(condition, table);
+}
